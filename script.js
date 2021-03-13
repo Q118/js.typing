cf84f5d61b00b83f84b94c6e532c4c03567d7626
Shelby Rothman <srothman@corelation.local>
2021-03-13 09:21:33 -0800

    async the quotes

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
 
 const quoteDisplayElement = document.getElementById('quoteDisplay')
 
+const quoteInputElement = document.getElementById('quoteInput')
+
+
 function getRandomQuote() {
     return fetch(RANDOM_QUOTE_API_URL)
         .then(response => response.json())
@@ -15,10 +18,17 @@ function getRandomQuote() {
 async function renderNewQuote() {
     const quote = await getRandomQuote()
 
-quoteDisplayElement.innerText = quote;
+    quoteDisplayElement.innerHTML = '';
 
-    console.log(quote)
-    
+//get each individual character of our string, creating a sapn for and and then setting the text of that span to that individual character
+quote.split('').forEach(character => {
+    const characterSpan = document.createElement('span')
+    characterSpan.innerText = character
+    quoteDisplayElement.appendChild(characterSpan)
+})
+
+    quoteInputElement.value = null
+
 }
 
 renderNewQuote()
