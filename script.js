2cfa188d79ed5cd92cef16d78ebe40a87c8e3ed0
Shelby Rothman <srothman@corelation.local>
2021-03-13 10:14:50 -0800

    code cleanup

--- script.js
+++ script.js
@@ -2,19 +2,14 @@
 //TOdo find soemthing that will include symbals and such for dev typers so code and such like random lines of code?
 
 const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
-
 const quoteDisplayElement = document.getElementById('quoteDisplay')
-
 const quoteInputElement = document.getElementById('quoteInput')
-
 const timerElement = document.getElementById('timer')
 
 //this input element or event listener gets called every single time something inside of the input box changes
 quoteInputElement.addEventListener('input', () => {
     const arrayQuote = quoteDisplayElement.querySelectorAll('span')
     const arrayValue = quoteInputElement.value.split('')
-
-
     let correct = true
     arrayQuote.forEach((characterSpan, index) => {
         const character = arrayValue[index]
@@ -31,7 +26,6 @@ quoteInputElement.addEventListener('input', () => {
             correct = false;
         }
     })
-
     if (correct) renderNewQuote()
 })
 
@@ -45,23 +39,17 @@ function getRandomQuote() {
 
 async function renderNewQuote() {
     const quote = await getRandomQuote()
-
     quoteDisplayElement.innerHTML = '';
 
     //get each individual character of our string, creating a sapn for and and then setting the text of that span to that individual character
     quote.split('').forEach(character => {
         const characterSpan = document.createElement('span')
-
-        //characterSpan.classList.add('incorrect')
-
         characterSpan.innerText = character
         quoteDisplayElement.appendChild(characterSpan)
     })
-
     quoteInputElement.value = null
     startTimer()
     //every new quote->restart the timer
-
 }
 
 let startTime;
