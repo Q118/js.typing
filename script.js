c9d2583a5db196bb3bb46cc11aca126deb2f34e0
Shelby Rothman <srothman@corelation.local>
2021-03-13 08:47:26 -0800

    functionality to fetch from API

---
+++ script.js
@@ -0,0 +1,11 @@
+//using the random quotes api for now
+//TOdo find soemthing that will include symbals and such for dev typers so code and such like random lines of code?
+
+const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+
+function getRandomQuote() {
+    return fetch(RANDOM_QUOTE_API_URL)
+        .then(response => response.json())
+        .then(data => data.content)
+
+}
